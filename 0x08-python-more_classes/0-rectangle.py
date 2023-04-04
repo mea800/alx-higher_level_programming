84c1a37fab1b9df8a5652ace10c35e9423c1e601
Mohamed Ezzat <mea80@yahoo.com>
2023-04-03 19:09:14 -0700

    task_0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """
-Defines an empty class Rectangle
+Defines the empty class Rectangle
 """
-
-class Rectangle(object):
-        """
-            An empty representation of a rectangle
-                """
-                    def __init__(self):
-                                pass
-
+class Rectangle:
+     """The Empty representation of a rectangle"""
+     pass
