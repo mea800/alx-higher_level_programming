64160d663adcd82d2311e2fbf509d2103a09c37b
mea800 <117851443+mea800@users.noreply.github.com>
2023-04-07 23:58:44 +0000

    task_8

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -3,10 +3,12 @@
 #include <Python.h>
 
 /**
- * print_python_string - Function that prints strings in Python
- * @p: Pointer to python object.
+ * print_python_string - prints information about a Python string object
+ * @p: pointer to a Python string object
  *
- * Return: No return.
+ * This function prints information about a Python string object, including its
+ * type, length, and value. If the input object is not a string, it prints an error
+ * message and returns.
  */
 void print_python_string(PyObject *p)
 {
@@ -14,18 +16,18 @@ void print_python_string(PyObject *p)
 
 	(void)repr;
 	printf("[.] string object info\n");
-
+	// Check if the input object is a string
 	if (strcmp(p->ob_type->tp_name, "str"))
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
-
+	// Determine if the string is compact ascii or compact unicode
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-
+	// Get the length and value of the string
 	repr = PyObject_Repr(p);
 	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
 	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
