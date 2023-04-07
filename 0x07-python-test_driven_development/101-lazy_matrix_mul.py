64d96e6d8f3dff381b395184466b64d9b11e14cb
mea800 <117851443+mea800@users.noreply.github.com>
2023-04-07 23:28:39 +0000

    task_7

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,12 +4,15 @@ This script defines a function that multiplies
 two matrices using the NumPy module.
 """
 
+
 import numpy as np
 
 
-def matrix_multiply(m_a, m_b):
+def lazy_matrix_mul(m_a, m_b):
     """
     This function takes two matrices as arguments and returns their product.
     """
     # Perform matrix multiplication using NumPy's matmul function
+    # m_a = ([1, 2], [3, 4])
+    # m_b = [[1, 2], [3, 4]]
     return np.matmul(m_a, m_b)
