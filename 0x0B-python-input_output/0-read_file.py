0d79554976bb5b9b6e30d4c0ac48427d5f58a341
mea800 <117851443+mea800@users.noreply.github.com>
2023-04-12 23:27:07 +0000

    task_0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Module containing the function read_file"""
+"""Module containing a function for reading file contents."""
 
 
 def read_file(filename=""):
-    """Reads a file and prints to stdout.
+    """Reads and prints contents of a file.
 
     Args:
         filename (str, optional): name of file to read. Defaults to "".
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
+        read_data = f.read()  # Read file contents
+        print(read_data, end='')  # Print file contents to stdout
