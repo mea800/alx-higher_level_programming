f31dee8c2d6a8a5b112ef5f484d536a53ddffd02
mea800 <117851443+mea800@users.noreply.github.com>
2023-04-12 23:53:08 +0000

    task_5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -21,3 +21,4 @@ def save_to_json_file(my_obj, filename):
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(json_object)
         # Close the file automatically with 'with' statement
+        f.close()
