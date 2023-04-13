9b29c740c1dcbc10c8d852c036b32cbac2d7b2c9
mea800 <117851443+mea800@users.noreply.github.com>
2023-04-13 01:41:20 +0000

    task_101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,17 +1,54 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write(
-        "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-                    random.randint(1, 255), random.randint(1, 255),
-                    random.randint(1, 255), random.randint(1, 255),
-                    datetime.datetime.now(),
-                    random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-                    random.randint(1, 1024)
-                        ))
-    sys.stdout.flush()
+import sys
+
+status_codes = {
+    '200': 0,  # Keep track of occurrences of HTTP 200 status code
+    '301': 0,  # Keep track of occurrences of HTTP 301 status code
+    '400': 0,  # Keep track of occurrences of HTTP 400 status code
+    '401': 0,  # Keep track of occurrences of HTTP 401 status code
+    '403': 0,  # Keep track of occurrences of HTTP 403 status code
+    '404': 0,  # Keep track of occurrences of HTTP 404 status code
+    '405': 0,  # Keep track of occurrences of HTTP 405 status code
+    '500': 0   # Keep track of occurrences of HTTP 500 status code
+}
+
+lc = 0  # Line counter
+file_size = 0  # Total file size counter
+
+
+def print_info():
+    """
+    Print file size and status code occurrences
+    """
+    print('File size: {:d}'.format(file_size))
+
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
+
+
+try:
+    for line in sys.stdin:
+        if lc != 0 and lc % 10 == 0:
+            print_info()
+
+        pieces = line.split()
+
+        try:
+            status = int(pieces[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+            pass
+
+        lc += 1
+
+    print_info()
+except KeyboardInterrupt:
+    print_info()
+    raise
