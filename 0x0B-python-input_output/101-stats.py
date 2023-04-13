73290ff0eddc55fdd614564282694eae2c907fab
mea800 <117851443+mea800@users.noreply.github.com>
2023-04-13 01:18:47 +0000

    task_101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,10 +7,13 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())  # Add random delay to simulate web server traffic
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \
+                     \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255),
+        random.randint(1, 255), random.randint(1, 255),
         datetime.datetime.now(),  # Add current timestamp for log entry
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),  # Generate random HTTP response code
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        # Generate random HTTP response code
         random.randint(1, 1024)  # Generate random response size
     ))
     sys.stdout.flush()  # Flush the log entry to stdout for immediate display
