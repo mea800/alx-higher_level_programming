07f3ce7522d8657faf7be4aeac162cbb96b9bc5f
mea800 <117851443+mea800@users.noreply.github.com>
2023-04-14 00:45:17 +0000

    task_6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class BaseGeometry based on 5-base_geometry.py"""
+"""Defines the BaseGeometry class with an area method."""
 
 
 class BaseGeometry:
-    """Class BaseGeometry.
+    """BaseGeometry class serves as a base class for other geometry classes.
     """
     def area(self):
-        """Area function.
+        """Area method for calculating the area of a shape.
 
         Raises:
-            Exception: if area is not implemented.
+            Exception: if area is not implemented in the subclass.
         """
         raise Exception("area() is not implemented")
